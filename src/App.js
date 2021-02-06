ae713d53a7026f7c2b6af22927299e110cacf712
Paras Verma <v.paras2@gmail.com>
2021-02-05 19:13:05 -0500

    Basic changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+    state = {
+        posts: [],
+
+    }
+    componentDidMount = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(responseData => {
+                const postsData = responseData.map(data => {
+                    return {
+                        ...data,
+                        show: false
+                    };
+                })
+                this.setState({ posts: postsData });
+
+            }
+            );
+
+    }
+
+    handleCollapsible = (id) => {
+        let postsData = [...this.state.posts];
+        postsData = postsData.map(data => {
+            const singleData = { ...data };
+            if (singleData.id === id) {
+                singleData.show = !singleData.show;
+            }
+
+            return singleData
+        })
+
+        this.setState({ posts: postsData })
+
+
+    }
+    render() {
+        const postData =
+            this.state.posts.map(data => (
+                <div className='collapsible' key={data.id}>
+                    <button 
+                        className={data.show? 'collapsible-button active' : 'collapsible-button'} 
+                        onClick={() => this.handleCollapsible(data.id)} 
+                        id={data.id}>{data.title}</button>
+                    <div className={data.show ? 'collapsible-data' : 'display'}>
+                        <p>{data.body}</p>
+                    </div>
+                </div>
+
+            )
+            )
+        return (
+            <div className='Container'>
+                <div className='ListBody'>
+                    {postData}
+                </div>
+            </div>
+        )
+
+    }
 }
-
 export default App;
